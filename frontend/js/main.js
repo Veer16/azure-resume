e72dc3bc45f06d2bf0a23d41324b1f003a859a79
Veer16 <veer.mailer@gmail.com>
2023-01-02 13:46:48 +1300

    new code added

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,18 +1,21 @@
 window.addEventListener('DOMContentLoaded',(event)=>{
     getVisitCount();
-})
-const functionAPI = '';
+});
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI).then(response =>{
-        return response.json;
-    }).then(response => {
-        console.log("Website called function API");
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerHTML = count;
-    }).catch(function(error){
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+        console.log("this is the error section")
+      });
     return count;
 }
